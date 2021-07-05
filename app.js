80e1a47e1d563dc3b0bd5566117d782f0c69615e
Blaine Curren <blaine.curren@gmail.com>
2021-07-05 18:24:57 -0500

    redo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const generatePage = require("src/page-template");
+const fs = require('fs');
+const generatePage = require('.src/page-template');
 
 const profileDataArgs = process.argv.slice(2);
 
@@ -11,8 +11,8 @@ console.log(name, github);
 
 const pageHTML = generatePage(name, github);
 
-fs.writeFile("./index.html", pageHTML, (err) => {
+fs.writeFile('./index.html', pageHTML, err => {
   if (err) throw err;
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
+  console.log('Portfolio complete! Check out index.html to see the output!');
 });
